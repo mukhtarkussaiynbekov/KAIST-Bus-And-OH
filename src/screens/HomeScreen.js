40425317b2965ee84a9d9ad48e97bca312d7b237
mukhtar <mukhtarkussaiynbekov@gmail.com>
2021-01-10 16:20:05 +0900

    center two buttons in home page

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,22 +1,32 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import { Button, ThemeProvider } from 'react-native-elements';
 
 const HomeScreen = ({ navigation }) => {
 	return (
-		<ThemeProvider>
-			<Button
-				title="Bus Timetable"
-				onPress={() => navigation.navigate('Bus')}
-			/>
-			<Button
-				title="Operating Hours"
-				onPress={() => navigation.navigate('OperatingHours')}
-			/>
-		</ThemeProvider>
+		<View style={styles.container}>
+			<ThemeProvider>
+				<Button
+					title="Bus Timetable"
+					onPress={() => navigation.navigate('Bus')}
+				/>
+				<Button
+					title="Operating Hours"
+					onPress={() => navigation.navigate('OperatingHours')}
+				/>
+			</ThemeProvider>
+		</View>
 	);
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		// alignItems: 'center',
+		justifyContent: 'center'
+		// borderColor: 'red',
+		// borderWidth: 10
+	}
+});
 
 export default HomeScreen;
