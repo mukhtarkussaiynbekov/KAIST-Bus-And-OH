fd953520f3c8513d4a94439b25b4a4e30cdf0432
mukhtar <mukhtarkussaiynbekov@gmail.com>
2021-01-10 16:59:35 +0900

    add icons to the buttons in home page

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
 import { Button, ThemeProvider } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const HomeScreen = ({ navigation }) => {
 	return (
 		<View style={styles.container}>
 			<ThemeProvider>
 				<Button
+					icon={<Icon name="bus" size={15} color="white" />}
+					iconContainerStyle={styles.icon}
 					title="Bus Timetable"
+					titleStyle={styles.title}
 					onPress={() => navigation.navigate('Bus')}
 				/>
 				<Button
+					icon={<Icon name="home" size={15} color="white" />}
+					iconContainerStyle={styles.icon}
 					title="Operating Hours"
 					onPress={() => navigation.navigate('OperatingHours')}
+					titleStyle={styles.title}
 				/>
 			</ThemeProvider>
 		</View>
@@ -26,6 +33,15 @@ const styles = StyleSheet.create({
 		justifyContent: 'center'
 		// borderColor: 'red',
 		// borderWidth: 10
+	},
+	icon: {
+		borderColor: 'yellow',
+		borderWidth: 10
+	},
+	title: {
+		borderColor: 'red',
+		borderWidth: 10,
+		marginLeft: 10
 	}
 });
 
