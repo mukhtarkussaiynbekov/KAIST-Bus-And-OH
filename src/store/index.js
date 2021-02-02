30988b7e9af38dd33092d8da9e4ad085d82adac0
mukhtar <mukhtarkussaiynbekov@gmail.com>
2021-02-02 20:17:37 +0900

    incorporate redux offline and thunk to the project

--- src/store/index.js
+++ src/store/index.js
@@ -1,16 +1,37 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore, persistReducer } from 'redux-persist';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
+
+import { createOffline } from '@redux-offline/redux-offline';
+import offlineConfig from '@redux-offline/redux-offline/lib/defaults/index';
 
 const persistConfig = {
 	key: 'root',
 	storage: AsyncStorage
 };
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const {
+	middleware: offlineMiddleware,
+	enhanceReducer: offlineEnhanceReducer,
+	enhanceStore: offlineEnhanceStore
+} = createOffline({
+	...offlineConfig,
+	persist: false
+});
 
-export const store = createStore(persistedReducer);
+const persistedReducer = persistReducer(
+	persistConfig,
+	offlineEnhanceReducer(rootReducer)
+);
+
+export const store = createStore(
+	persistedReducer,
+	composeWithDevTools(
+		offlineEnhanceStore,
+		applyMiddleware(thunk, offlineMiddleware)
+	)
+);
 export const persistor = persistStore(store);
-// TODO: apply thunk middleware to incorporate firebase. async calls need thunk middleware
