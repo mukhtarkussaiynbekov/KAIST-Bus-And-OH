9516f254d48ce5b1d2865041ffce9258e2d99597
mukhtar <mukhtarkussaiynbekov@gmail.com>
2021-03-12 10:51:41 +0900

    update home screen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -260,7 +260,7 @@ const styles = StyleSheet.create({
 		paddingHorizontal: 10,
 		textAlign: 'center',
 		marginVertical: 10,
-		fontSize: 16
+		fontSize: 18
 	},
 	boldText: {
 		fontWeight: 'bold'
